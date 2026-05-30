866dad74bc144e53463f3738e5dbc6c3a81e38ed
Alpha Oumar Diallo <dalphaoumar27000@gmail.com>
2026-05-30 16:17:19 +0000

    git commit -m "feat: add pagination to products endpoint"

--- src/main/java/com/payflash/controller/ProductController.java
+++ src/main/java/com/payflash/controller/ProductController.java
@@ -5,6 +5,9 @@ import com.payflash.dto.ProductResponseDto;
 import com.payflash.model.Product;
 import com.payflash.service.ProductService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,8 +23,12 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<ProductResponseDto> getProducts(){
-        return productService.getProducts();
+    public ResponseEntity<Page<ProductResponseDto>> getProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(productService.getProducts(pageable));
     }
 
     @PostMapping
